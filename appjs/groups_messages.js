d8d39f97c96feb64df0c9c378d9636fc1f6a580f
rotcehxp <hector.diaz6@upr.edu>
2018-05-01 23:49:31 -0400

    Messages now displays info, date, likes and dislikes.

--- appjs/groups_messages.js
+++ appjs/groups_messages.js
@@ -64,7 +64,7 @@ angular.module('MessageApp').controller('GroupsMessagesController', ['$http', '$
             var content = document.getElementById("content");
 
             if(sender && content != null){
-                $("#chat").append("<tr><th>Sent by: </th><th>"+sender.value+"</th></tr><tr><th>Content: </th><th>"+content.value+"</th></tr><tr><th>Date: </th><th>"+new Date()+"</th></tr>");
+                $("#chat").append("<tr><th>Sent by: </th><th>"+sender.value+"</th></tr><tr><th>Content: </th><th>"+content.value+"</th></tr><tr><th>Date: </th><th>"+new Date()+"</th></tr><tr style='border-bottom: solid bold green'><th>Likes: </th><th>0</th></tr><tr style='border-bottom: solid bold green'><th>Dislikes: </th><th>0</th></tr>");
             }
 
             /*
@@ -112,6 +112,56 @@ angular.module('MessageApp').controller('GroupsMessagesController', ['$http', '$
                 );
             }*/
         };
+
+
+        this.loadNumberOfLikes = function(mid){
+            // Get the target part id from the parameter in the url
+            // using the $routerParams object
+            var gid = $routeParams.gid;
+
+            console.log(mid);
+
+            // Now create the url with the route to talk with the rest API
+            var reqURL = "http://localhost:5000/MessageApp/messages/"+mid+"/number-of-likes";
+            console.log("reqURL: " + reqURL);
+            // Now issue the http request to the rest API
+            $http.get(reqURL).then(
+                // Success function
+                function (response) {
+                    console.log("data: " + JSON.stringify(response.data));
+                    // assing the part details to the variable in the controller
+                    thisCtrl.likes = response.data.Likes[0];
+                    console.log(response.data.Likes[0]);
+
+                }, //Error function
+                function (response) {
+                    // This is the error function
+                    // If we get here, some error occurred.
+                    // Verify which was the cause and show an alert.
+                    var status = response.status;
+                    //console.log("Error: " + reqURL);
+                    //alert("Cristo");
+                    if (status == 0) {
+                        alert("No hay conexion a Internet");
+                    }
+                    else if (status == 401) {
+                        alert("Su sesion expiro. Conectese de nuevo.");
+                    }
+                    else if (status == 403) {
+                        alert("No esta autorizado a usar el sistema.");
+                    }
+                    else if (status == 404) {
+                        alert("No se encontro la informacion solicitada.");
+                    }
+                    else {
+                        alert("Error interno del sistema.");
+                    }
+                }
+            );
+            return thisCtrl.likes;
+        };
+
+
 
         this.loadGroupMessages();
 }]);
