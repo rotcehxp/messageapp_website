e53bfec5c4534c74f82be868786c2074e575cf67
Hector Luis <rotcehxp@MacBook-Pro-de-Hector.local>
2018-05-08 08:44:09 -0400

    Likes and dislikes work separated.

--- appjs/groups_messages.js
+++ appjs/groups_messages.js
@@ -161,6 +161,54 @@ angular.module('MessageApp').controller('GroupsMessagesController', ['$http', '$
             return thisCtrl.likes;
         };
 
+        this.loadNumberOfDislikes = function(mid){
+            // Get the target part id from the parameter in the url
+            // using the $routerParams object
+            var gid = $routeParams.gid;
+
+            console.log(mid);
+
+            // Now create the url with the route to talk with the rest API
+            var reqURL = "http://localhost:5000/MessageApp/messages/"+mid+"/number-of-dislikes";
+            console.log("reqURL: " + reqURL);
+            // Now issue the http request to the rest API
+            $http.get(reqURL).then(
+                // Success function
+                function (response) {
+                    console.log("data: " + JSON.stringify(response.data));
+                    // assing the part details to the variable in the controller
+                    thisCtrl.dislikes = response.data.Likes[0];
+                    console.log(response.data.Dislikes[0]);
+
+                }, //Error function
+                function (response) {
+                    // This is the error function
+                    // If we get here, some error occurred.
+                    // Verify which was the cause and show an alert.
+                    var status = response.status;
+                    //console.log("Error: " + reqURL);
+                    //alert("Cristo");
+                    if (status == 0) {
+                        alert("No hay conexion a Internet");
+                    }
+                    else if (status == 401) {
+                        alert("Su sesion expiro. Conectese de nuevo.");
+                    }
+                    else if (status == 403) {
+                        alert("No esta autorizado a usar el sistema.");
+                    }
+                    else if (status == 404) {
+                        alert("No se encontro la informacion solicitada.");
+                    }
+                    else {
+                        alert("Error interno del sistema.");
+                    }
+                }
+            );
+            return thisCtrl.dislikes;
+        };
+
+
 
 
         this.loadGroupMessages();
